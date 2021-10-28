908b4613ffffd6f2a035ec3481ed4e087a71ebf0
Moruf Ajibike <abiodun.ajibike1@yahoo.com>
2021-10-28 11:26:24 +0100

    Update provider config

--- challenge-1/providers.tf
+++ challenge-1/providers.tf
@@ -1,9 +1,16 @@
 #
 # Provider Configuration
 #
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
 
 provider "aws" {
   region  = "eu-west-1"
   profile = "development"
-  version = ">= 2.57"
 }
